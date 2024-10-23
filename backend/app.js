63534c5e15d93b393835c13ec0a25d2b9ad01dac
23MorganDev <aprilballoon333@gmail.com>
2024-10-23 21:57:47 +0300

    cors fixes

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,26 @@ const connectDB = require("./config/db");
 const app = express();
 const cors = require("cors");
 
-connectDB();
+// Connect to the database
+connectDB().catch((err) => {
+  console.error("Failed to connect to DB", err);
+  process.exit(1);
+});
+
+// Configure CORS options
+const corsOptions = {
+  origin: 'https://foodie-6ykt.onrender.com', //frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, 
+};
 
 // Initialize middleware
-//increase the size limit of formdata
-app.use(express.json({ limit: '10mb' })); //for json requests
-app.use(express.urlencoded({ limit: '10mb', extended: true }));  //for urlencoded request
-app.use(cors());
-app.use("/uploads", express.static("uploads"));
+app.use(cors(corsOptions)); 
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true })); 
+app.use("/uploads", express.static("uploads")); 
 
-// Define routes
+// Routes
 const users = require("./routes/users");
 const recipes = require("./routes/recipes");
 const ratings = require("./routes/ratings");
@@ -21,8 +31,8 @@ app.use("/backend/users", users);
 app.use("/backend/recipes", recipes);
 app.use("/backend/ratings", ratings);
 
-const PORT = process.env.PORT || 5000;
-
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
